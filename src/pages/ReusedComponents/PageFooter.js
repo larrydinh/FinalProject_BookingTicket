c63113735373468452fe6f652d25f3f842e5936e
Larry <huuphucdinh@gmail.com>
2021-04-26 00:03:06 +0700

    day1_tailwind_installed

--- src/pages/ReusedComponents/PageFooter.js
+++ src/pages/ReusedComponents/PageFooter.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import logo from "../../assets/images/logo.png";
 
 export default function PageFooter() {
-    return (
-        <div>
-            footer
-        </div>
-    )
+  return <div className="container">
+      <img src= {logo} alt=""/>
+     </div>;
 }
--- src/pages/ReusedComponents/PageHeader.js
+++ src/pages/ReusedComponents/PageHeader.js
@@ -1,215 +1,197 @@
 import React from "react";
+// Import pictures
+import logo from "../../assets/images/logo.png";
 
 export default function PageHeader() {
   return (
-    <div>
-      <header className="ht-header">
-        <div className="container">
-          <nav className="navbar navbar-default navbar-custom">
-            <div className="navbar-header logo">
-              <div
-                className="navbar-toggle"
-                data-toggle="collapse"
-                data-target="#bs-example-navbar-collapse-1"
-              >
-                <span className="sr-only">Toggle navigation</span>
-                <div id="nav-icon1">
-                  <span />
-                  <span />
-                  <span />
-                </div>
-              </div>
-              <a href="index.html">
-                <img
-                  className="logo"
-                  src="images/logo1.png"
-                  alt
-                  width={119}
-                  height={58}
-                />
-              </a>
-            </div>
-            <div
-              className="collapse navbar-collapse flex-parent"
-              id="bs-example-navbar-collapse-1"
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="relative flex items-center justify-between h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
             >
-              <ul className="nav navbar-nav flex-child-menu menu-left">
-                <li className="hidden">
-                  <a href="#page-top" />
-                </li>
-                <li className="dropdown first">
-                  <a
-                    className="btn btn-default dropdown-toggle lv1"
-                    data-toggle="dropdown"
-                  >
-                    Home <i className="fa fa-angle-down" aria-hidden="true" />
-                  </a>
-                  <ul className="dropdown-menu level1">
-                    <li>
-                      <a href="index.html">Home 01</a>
-                    </li>
-                    <li>
-                      <a href="homev2.html">Home 02</a>
-                    </li>
-                    <li>
-                      <a href="homev3.html">Home 03</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="dropdown first">
-                  <a
-                    className="btn btn-default dropdown-toggle lv1"
-                    data-toggle="dropdown"
-                    data-hover="dropdown"
-                  >
-                    movies
-                    <i className="fa fa-angle-down" aria-hidden="true" />
-                  </a>
-                  <ul className="dropdown-menu level1">
-                    <li className="dropdown">
-                      <a
-                        href="#"
-                        className="dropdown-toggle"
-                        data-toggle="dropdown"
-                      >
-                        Movie grid
-                        <i className="ion-ios-arrow-forward" />
-                      </a>
-                      <ul className="dropdown-menu level2">
-                        <li>
-                          <a href="moviegrid.html">Movie grid</a>
-                        </li>
-                        <li>
-                          <a href="moviegridfw.html">movie grid full width</a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
-                      <a href="movielist.html">Movie list</a>
-                    </li>
-                    <li>
-                      <a href="moviesingle.html">Movie single</a>
-                    </li>
-                    <li className="it-last">
-                      <a href="seriessingle.html">Series single</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="dropdown first">
-                  <a
-                    className="btn btn-default dropdown-toggle lv1"
-                    data-toggle="dropdown"
-                    data-hover="dropdown"
-                  >
-                    celebrities{" "}
-                    <i className="fa fa-angle-down" aria-hidden="true" />
-                  </a>
-                  <ul className="dropdown-menu level1">
-                    <li>
-                      <a href="celebritygrid01.html">celebrity grid 01</a>
-                    </li>
-                    <li>
-                      <a href="celebritygrid02.html">celebrity grid 02 </a>
-                    </li>
-                    <li>
-                      <a href="celebritylist.html">celebrity list</a>
-                    </li>
-                    <li className="it-last">
-                      <a href="celebritysingle.html">celebrity single</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="dropdown first">
-                  <a
-                    className="btn btn-default dropdown-toggle lv1"
-                    data-toggle="dropdown"
-                    data-hover="dropdown"
-                  >
-                    news <i className="fa fa-angle-down" aria-hidden="true" />
-                  </a>
-                  <ul className="dropdown-menu level1">
-                    <li>
-                      <a href="bloglist.html">blog List</a>
-                    </li>
-                    <li>
-                      <a href="bloggrid.html">blog Grid</a>
-                    </li>
-                    <li className="it-last">
-                      <a href="blogdetail.html">blog Detail</a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="dropdown first">
-                  <a
-                    className="btn btn-default dropdown-toggle lv1"
-                    data-toggle="dropdown"
-                    data-hover="dropdown"
-                  >
-                    community{" "}
-                    <i className="fa fa-angle-down" aria-hidden="true" />
-                  </a>
-                  <ul className="dropdown-menu level1">
-                    <li>
-                      <a href="userfavoritegrid.html">user favorite grid</a>
-                    </li>
-                    <li>
-                      <a href="userfavoritelist.html">user favorite list</a>
-                    </li>
-                    <li>
-                      <a href="userprofile.html">user profile</a>
-                    </li>
-                    <li className="it-last">
-                      <a href="userrate.html">user rate</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-              <ul className="nav navbar-nav flex-child-menu menu-right">
-                <li className="dropdown first">
-                  <a
-                    className="btn btn-default dropdown-toggle lv1"
-                    data-toggle="dropdown"
-                    data-hover="dropdown"
-                  >
-                    pages <i className="fa fa-angle-down" aria-hidden="true" />
-                  </a>
-                  <ul className="dropdown-menu level1">
-                    <li>
-                      <a href="landing.html">Landing</a>
-                    </li>
-                    <li>
-                      <a href="404.html">404 Page</a>
-                    </li>
-                    <li className="it-last">
-                      <a href="comingsoon.html">Coming soon</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a href="#">Help</a>
-                </li>
-                <li className="loginLink">
-                  <a href="#">LOG In</a>
-                </li>
-                <li className="btn signupLink">
-                  <a href="#">sign up</a>
-                </li>
-              </ul>
+              <span className="sr-only">Open main menu</span>
+
+              <svg
+                className="block h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+              <svg
+                className="hidden h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex-shrink-0 flex items-center">
+              <img
+                className="hidden lg:block h-8 w-auto"
+                src={logo}
+                alt="page logo"
+              />
             </div>
-          </nav>
-          {/* top search form */}
-          <div className="top-search">
-            <select>
-              <option value="united">TV show</option>
-              <option value="saab">Others</option>
-            </select>
-            <input
-              type="text"
-              placeholder="Search for a movie, TV Show or celebrity that you are looking for"
-            />
+            <div className="hidden sm:block sm:ml-6">
+              <div className="flex space-x-4">
+                <a
+                  href="#"
+                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  aria-current="page"
+                >
+                  Home
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Movie
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Contact
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  News
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <button className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+              <span className="sr-only">View notifications</span>
+
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+            </button>
+            <div className="ml-3 relative">
+              <div>
+                <button
+                  type="button"
+                  className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  id="user-menu-button"
+                  aria-expanded="false"
+                  aria-haspopup="true"
+                >
+                  <span className="sr-only">Open user menu</span>
+                  <img
+                    className="h-8 w-8 rounded-full"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt
+                  />
+                </button>
+              </div>
+
+              {/* <div
+                className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="user-menu-button"
+                tabIndex={-1}
+              >
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700"
+                  role="menuitem"
+                  tabIndex={-1}
+                  id="user-menu-item-0"
+                >
+                  Your Profile
+                </a>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700"
+                  role="menuitem"
+                  tabIndex={-1}
+                  id="user-menu-item-1"
+                >
+                  Settings
+                </a>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700"
+                  role="menuitem"
+                  tabIndex={-1}
+                  id="user-menu-item-2"
+                >
+                  Sign out
+                </a>
+              </div> */}
+            </div>
           </div>
         </div>
-      </header>
-    </div>
+      </div>
+      <div className="sm:hidden" id="mobile-menu">
+        <div className="px-2 pt-2 pb-3 space-y-1">
+          <a
+            href="#"
+            className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+            aria-current="page"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Movie
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            Contact
+          </a>
+          <a
+            href="#"
+            className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+          >
+            News
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 }
